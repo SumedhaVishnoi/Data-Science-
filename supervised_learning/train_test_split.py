9513e08ee8731323c484432462a72306652a918b
SumedhaVishnoi <113627960+SumedhaVishnoi@users.noreply.github.com>
2025-08-23 16:22:13 +0530

    Add files via upload

---
+++ supervised_learning/train_test_split.py
@@ -0,0 +1,10 @@
+'''In supervised learning, we need to check how well our model generalizes to unseen data.
+
+If we train & test on the same data → model will just memorize (overfitting).
+
+So we split dataset into:
+
+Training Set → used to train the model
+
+Test Set → used to evaluate the model
+'''
